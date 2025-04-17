45ef64c81a0a070e7e76f60f8c3b76447a06f2e3
jjjoe1010 <jjjoe1010@gmail.com>
2025-04-17 19:11:33 +0100

    formating website added title, user name and search bar added in function handleSearch creating search function into website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,32 @@
+"use client"
 import Image from "next/image";
-
+import { useState } from "react";
 export default function Home() {
+  const [search, setSearch] = useState("");
+
+  function handleSearch(formData:FormData){
+    const query = formData.get("query");
+    if (query != null){
+      setSearch(query.toString())      
+    }
+  }
+
   return (
     <section>
-      <div>
+      <div className="flex justify-between p-10">
         <div>
-          <h1>Recipe Record Log</h1>
+          <h1 className="text-2xl">Recipe Records Log</h1>
         </div>
         <div>
-          <p>username</p>
+          <p className="text-lg">username</p>
+        </div>
+      </div>
+      <div>
+        <div className="flex justify-end pt-20 pr-14 pb-10">
+          <form action={handleSearch}>
+            <input name="query" />
+            <button type="submit">Search</button>
+          </form>
         </div>
       </div>
     </section>
