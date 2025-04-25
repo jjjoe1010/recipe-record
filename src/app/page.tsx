bf14644b0b0b26a77f8713f433b573c9416be0c6
jjjoe1010 <jjjoe1010@gmail.com>
2025-04-25 22:27:56 +0100

    create button that add recipe (but no function yet) styled the button created a bigger list of recipies for testing call storage created newdiv call thread and tbody

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 import Image from "next/image";
+import { storedRecipe } from "./storage"
 import { useState } from "react";
 export default function Home() {
   const [search, setSearch] = useState("");
+  const [recipes, setRecipes] = useState(storedRecipe);
 
   function handleSearch(formData:FormData){
     const query = formData.get("query");
@@ -11,6 +13,26 @@ export default function Home() {
     }
   }
 
+  function createRecipe(){
+    console.log("create new recipe")
+  }
+
+  function handleSort(header:string){
+    // if (header == "recipe"|| header == "origin"){
+
+    // }
+    // recipes.sort((a,b) => {
+    //   const nameA = a.recipeName.toUpperCase();
+    //   const nameB = b.recipeName.toUpperCase();
+    //   if (nameA < nameB) {
+    //     return -1;
+    //   }
+    //   if (nameA > nameB) {
+    //     return 1;
+    //   }
+    //   return 0;
+    // )}
+  }
   return (
     <section>
       <div className="flex justify-between p-10">
@@ -31,21 +53,29 @@ export default function Home() {
       </div>
       <div className="px-10">
         <table className="table-fixed min-w-full">
-          <tr className="text-left bg-gray-400/80">
-            <th>Recipe</th>
-            <th>Origin</th>
-            <th>Date Created</th>
-            <th>Rating</th>
-          </tr>
-          <tr className=" hover:bg-gray-300">
-            <td className="text-left">Gyu Don</td>
-            <td className="text-left">Japanese</td>
-            <td className="text-left">17/04/25</td>
-            <td className="text-left">8/10</td>
-          </tr>
+          <thead>
+            <tr className="text-left bg-gray-400/80">
+              <th onClick={()=>handleSort("recipe")}>Recipe</th>
+              <th onClick={()=>handleSort("origin")}>Origin</th>
+              <th onClick={()=>handleSort("dateCreated")}>Date Created</th>
+              <th onClick={()=>handleSort("rating")}>Rating</th>
+            </tr>            
+          </thead>
+          <tbody>
+            {recipes.map((item) => (
+            <tr className=" hover:bg-gray-300" key={item.id}>
+              <td className="text-left">{item.recipeName}</td>
+              <td className="text-left">{item.origin}</td>
+              <td className="text-left">{item.dateCreated}</td>
+              <td className="text-left">{item.rating}</td>
+            </tr>              
+            ))}
+          </tbody>
         </table>
+      </div>
+      <div className="flex justify-end pr-14">
+        <button type="button" onClick={createRecipe} className="pl-2 pr-2 rounded-full hover:bg-gray-300"> + Add recipe </button>
       </div>
     </section>
   );
 }
-[]
